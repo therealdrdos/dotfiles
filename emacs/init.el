d57660948846ea2f061a7113f55952770e4115f9
therealdrdos <s1monp@mailbox.org>
2025-11-08 21:45:18 +0100

    chore(emacs): update package-selected-packages

--- emacs/init.el
+++ emacs/init.el
@@ -436,7 +436,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company-web dashboard dockerfile-mode doom-modeline emmet-mode
+		 fireplace flycheck-checkbashisms flycheck-eglot
+		 flycheck-rust hl-todo magit marginalia markdown-mode
+		 nerd-icons-dired org-superstar ox-rss php-mode
+		 reformatter rust-mode solarized-theme toc-org vertico
+		 yaml-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
